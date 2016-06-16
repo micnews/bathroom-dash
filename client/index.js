ef692a61211f3855ad159a34f72f68fda3603db3
ryanscottaudio <iamryancampbell@gmail.com>
2016-06-16 16:26:24 +0200

    Some stuff

--- client/index.js
+++ client/index.js
@@ -37,7 +37,6 @@ const playListener = function (e) {
     return;
   }
 
-
   playButton.removeEventListener('click', playListener);
   window.removeEventListener('resize', resetPlayable);
   window.removeEventListener('orientationchange', resetPlayable);
